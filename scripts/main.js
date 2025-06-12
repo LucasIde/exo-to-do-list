0c22c434843952236a452d690741ec09cba9ed90
lucas ide <lucaside000@gmail.com>
2025-06-12 12:04:23 +0200

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -28,20 +28,28 @@ const toDo = [];
 // 🎊 Fonctionnalités
 // ==============================
 
+function dateFormat(date) {
+    const tmp = date.split("-");
+    const swap = tmp[0];
+    tmp[0] = tmp[2];
+    tmp[2] = swap;
+    return (tmp.join("/"));
+}
+
 function createTask(obj, node) {
-    let len = (node) ? toDo.length - 1 : done.length - 1;3
-    console.log(done);
-    console.log(done.length - 1);
+    let len = (node) ? toDo.length - 1 : done.length - 1;
+    let check = (node) ? "" : "checked";
+    const formatedDate = dateFormat(obj.date)
     const details = document.createElement("details");
     details.classList.add("task");
     details.id = len;
     details.innerHTML = `
     <summary>
         <div class="wrapperSum">
-            <input type="checkbox">
+            <input type="checkbox" ${check}>
             <div class="txt">
                 <div class="title">${obj.name}</div>
-                <div class="date">${obj.date}</div>
+                <div class="date">${formatedDate}</div>
             </div>
         </div>
     </summary>
@@ -56,16 +64,29 @@ function changeindex(querry) {
     });
 }
 
-function removeTask(id, array, querry) {
-    array.splice(id, 1);
+function removeTask(id, array, qClass) {
+    const obj = array.splice(id, 1);
+    const querry = document.querySelectorAll(qClass);
     if (querry) {
         changeindex(querry);
     }
+    return obj[0];
 }
 
 function addTask(obj, node) {
     const taskElement = createTask(obj, node);
     listHTML[node].append(taskElement);
+}
+
+function sortDate(a, b) {
+    const x = new Date(a.date).getTime();
+    const y = new Date(b.date).getTime();
+    if (x < y) {
+        return (-1);
+    }
+    else {
+        return (1);
+    }
 }
 
 // ==============================
@@ -76,27 +97,37 @@ addBtn.addEventListener("click", ()=> {
     if (nameInput.value && dateInput.value && descInput.value) {
         const newTask = new task(nameInput.value, dateInput.value, descInput.value);
         toDo.push(newTask);
-        console.log(toDo);
         addTask(newTask, 1);
+        console.log();
+
     }
 })
 
 listHTML.forEach(element => {
     element.addEventListener("change", (e)=> {
-        const id = e.target.closest(".task").id
+        console.log(toDo);
+        console.log(done);
+        const id = e.target.closest(".task").id;
         if (e.target.checked) {
-            done.push(toDo[id]);
-            addTask(toDo[id], 0);
             e.target.closest(".task").remove();
-            const querry = document.querySelectorAll(".to-do .task");
-            removeTask(id,toDo, querry);
+            const obj = removeTask(id,toDo, ".to-do .task");
+            done.push(obj);
+            addTask(obj, 0);
         }
         else {
-            toDo.push(done[id]);
-            addTask(done[id], 1);
             e.target.closest(".task").remove();
-            const querry = document.querySelectorAll(".done .task");
-            removeTask(id,done, querry);
+            const obj = removeTask(id,done, ".done .task");
+            toDo.push(obj);
+            addTask(obj, 1);
         }
     })
 });
+
+triBtn.addEventListener("click", ()=> {
+    toDo.sort(sortDate)
+    listHTML[1].innerHTML = "";
+    toDo.forEach(obj => {
+        addTask(obj, 1);
+    })
+    changeindex(document.querySelectorAll(".to-do .task"));
+})
